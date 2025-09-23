d17b5226d43c630be401d0733391b9cda24a7c35
Lawrence Salazar <lawrence.c.salazar@gmail.com>
2025-09-24 01:42:58 +0800

    Main.py firebase API

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 import xgboost as xgb
 
 # Firebase
